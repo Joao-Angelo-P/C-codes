9dcaf60fe0732756117a9c48768613a35f8650e4
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2026-01-04 13:13:29 -0400

    Fix palindrome check for zero and negative numbers

--- palindrome.c
+++ palindrome.c
@@ -1,8 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+
 bool isPalindrome(int x) {
     unsigned int rev = 0;
     int num = x;
     // int negative = false;
-    if (x < 0 || !(x % 10) & x > 0){
+    if (x < 0 || !(x % 10 && x != 0)){
         return false;
     };
     while (x){
