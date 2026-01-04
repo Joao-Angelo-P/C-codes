60975d91c87b95fe86b5922865f500a1025fb0df
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2026-01-04 13:08:18 -0400

    Implement palindrome check function

---
+++ palindrome.c
@@ -0,0 +1,14 @@
+bool isPalindrome(int x) {
+    unsigned int rev = 0;
+    int num = x;
+    // int negative = false;
+    if (x < 0 || !(x % 10) & x > 0){
+        return false;
+    };
+    while (x){
+        rev = rev * 10 + x % 10;
+        x /= 10; 
+    };
+
+    return rev == num;
+}
